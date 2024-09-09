ac206f3b9e0e8a9798d3addd7faecc26c71417ed
jamesnulliu <jamesnulliu@gmail.com>
2024-09-09 23:57:39 +0800

    Update Some CMake Function Names to Avoid Conflits with Other Projects; Update Build Scripts

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -1,4 +1,34 @@
-SPDLOG_HOME="$(pwd)/build/externals/spdlog/install"
+git submodule update --init
+
+# IF SPDLOG_HOME is not set:
+if [ -z "$SPDLOG_HOME" ]; then
+    echo "SPDLOG_HOME is not set. Checking \"build/externals/spdlog/install\"..."
+    SupposedSpdlogHome="$(pwd)/build/externals/spdlog/install"
+    if [ -d $SupposedSpdlogHome ]; then
+        SPDLOG_HOME=$SupposedSpdlogHome
+    else
+        echo "SPDLOG_HOME is not set and \"$SupposedSpdlogHome\" does not exist."
+        echo "Using \"./scripts/install-spdlog.sh\" to clone, build and install spdlog as a git submodule."
+        # Clone, build and install.
+        ./scripts/install-spdlog.sh
+        if [ -d $SupposedSpdlogHome ]; then
+            SPDLOG_HOME=$SupposedSpdlogHome
+        else
+            echo "Installation of submodule spdlog failed. Can't find a valid SPDLOG_HOME."
+            echo "Aborting."
+            exit 1
+        fi
+    fi
+else 
+    # Simply trust that your $SPDLOG_HOME is valid.
+    echo "Found system variable SPDLOG_HOME: $SPDLOG_HOME"
+    # If the path does not exist, exit.
+    if [ ! -d $SPDLOG_HOME ]; then
+        echo "The path does not exist. Can't fina a valid SPDLOG_HOME."
+        echo "Aborting."
+        exit 1
+    fi
+fi
 
 cmake -G Ninja -S . -B ./build \
     -DCMAKE_BUILD_TYPE=Release \
