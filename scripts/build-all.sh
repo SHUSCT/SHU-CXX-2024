e86ee89836fae8cce1e00eb335fc593b1ff23cde
jamesnulliu <jamesnulliu@gmail.com>
2024-09-12 03:17:55 +0000

    Update Submodules

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -1,4 +1,4 @@
-git submodule update --init
+git submodule update --remote
 
 # IF: SPDLOG_HOME is not set or does not exist, build and install spdlog:
 if [ -z "$SPDLOG_HOME" ] || [ ! -d "$SPDLOG_HOME" ]; then
